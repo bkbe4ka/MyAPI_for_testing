899126e04f204908ed251d4119511f71e5c24419
Gleb <gleb22ivak@gmail.com>
2026-08-03 13:10:16 +0300

    add date validator

--- app/schemas.py
+++ app/schemas.py
@@ -8,18 +8,18 @@ class BookingCreate(BaseModel):
 
     firstname: str = Field(min_length = 1, max_length = 50)
     lastname: str = Field(min_length = 1, max_length = 50)
-    totalprice: int = Field(ge=0)
+    totalprice: int = Field(ge=0, le=2_147_483_647)
     depositpaid: bool
-    checkin: date
-    checkout: date
+    checkin: date = Field(strict=False)
+    checkout: date = Field(strict=False)
     additionalneeds: str | None = Field(default=None, max_length=200)
 
-    @model_validator(mode='after')
-    def checkout_after_ckeckin(self):
-        if self.checkout <= self.checkin:
-            raise ValueError('Checkout must be after checkin')
-        return self
-
+    @field_validator('checkin', 'checkout', mode='before')
+    @classmethod
+    def date_must_be_str(cls, v):
+        if not isinstance(v, str):
+            raise ValueError("Date must be a string in YYYY-MM-DD format")
+        return v
 
     @field_validator('firstname', 'lastname', 'additionalneeds')
     @classmethod
@@ -27,6 +27,12 @@ class BookingCreate(BaseModel):
         if v is not None and '\x00' in v:
             raise ValueError('Field must not contain NUL characters')
         return v
+
+    @model_validator(mode='after')
+    def checkout_after_ckeckin(self):
+        if self.checkout <= self.checkin:
+            raise ValueError('Checkout must be after checkin')
+        return self
 
 class BookingResponse(BaseModel):
     model_config = ConfigDict(from_attributes = True)
@@ -39,3 +45,5 @@ class BookingResponse(BaseModel):
     checkin: date
     checkout: date
     additionalneeds: str | None
+
+
